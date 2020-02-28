1c1a3023cafad6d82bb88ebdf5376d29a172ceb2
Oliver Rakos <rakosoliver827@gmail.com>
2020-02-28 11:18:55 +0100

    file and function plans for ploting the trajectories

---
+++ core/utils/trajectory_plot.py
@@ -0,0 +1,20 @@
+import os
+import torch
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from core.common import *
+
+def plot_target_pred(target:Trajectories, prediction:Trajectories):
+
+
+def load_files():
+    path = RESULTS_PATH
+
+
+def prepare(targ, preg):
+    return target, prediction
+
+
+if __name__ == '__main__':
